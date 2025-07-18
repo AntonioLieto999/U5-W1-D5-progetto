af4f39c0a4a1163e6c1d3416f464200f42b50420
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-07-18 12:29:27 +0200

    implementazione del runner
    fatto un package con il runner all'interno (non completo)

---
+++ src/main/java/Antonio/U5_W1_D5_progetto/runner/RunnerApplication.java
@@ -0,0 +1,13 @@
+package Antonio.U5_W1_D5_progetto.runner;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunnerApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
