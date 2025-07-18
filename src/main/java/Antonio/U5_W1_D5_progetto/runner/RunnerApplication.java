f297b4ad5d3d78ec03ab38139be246be3ec35768
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-07-18 16:24:29 +0200

    aggiunta del drawSQL

--- src/main/java/Antonio/U5_W1_D5_progetto/runner/RunnerApplication.java
+++ src/main/java/Antonio/U5_W1_D5_progetto/runner/RunnerApplication.java
@@ -60,5 +60,4 @@ public class RunnerApplication implements CommandLineRunner {
             System.out.println("Prenotazione fallita per " + u.getUsername() + ": " + e.getMessage());
         }
     }
-
 }
